09b5225c297d89afdb407d42182ed9d9389e24b2
Da-ntas <hugodfl2002@gmail.com>
2023-01-31 22:43:29 -0300

    Correção tamanho titulo do diario

--- src/components/Diarios/index.js
+++ src/components/Diarios/index.js
@@ -16,7 +16,7 @@ const Diarios = ({...props}) => {
                 : <div style={{width: 150, height: 150, background: props.image ? `url(${props.image})` : props.color, border: '1px solid #F7F6F2', filter: 'drop-shadow(3px 3px 2px #1B262C)'}}></div>
                 }
             </Row>
-            <Row style={{fontSize: '1.4rem', maxWidth: 125}} className="mt-2 user-select-none diario-titulo">
+            <Row style={{fontSize: '1.4rem', maxWidth: 140}} className="mt-2 user-select-none diario-titulo">
                 {props.title}
             </Row>
             <Row className="w-100">
